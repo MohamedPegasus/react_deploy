046d77358845b735ebf48992da5c869774e782a2
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-16 15:35:59 +0200

    kg changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import Header from './componenets/Header';
 import Home from './componenets/Home';
 import Contact from './componenets/Contact';
 import Pricing from './componenets/Pricing';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes as Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
       <Router>
       <Header />
-      <Routes>
+      <Switch>
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/contact' element={<Contact />}></Route>
         <Route exact path='/pricing' element={<Pricing />}></Route>
-      </Routes>
-        
+      </Switch>
       </Router>
     
   );
