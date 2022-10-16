3e4d16c811d9df2f833904b440c3214a7905b424
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-16 15:14:33 +0200

    react_deploy

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Header from './componenets/Header';
 import Home from './componenets/Home';
 import Contact from './componenets/Contact';
 import Pricing from './componenets/Pricing';
-import {BrowsrRouter, Routes, Route} from 'react-router-dom';
+import {BrowsrRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowsrRouter>
+      <Router>
       <Header />
       <Routes>
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/' element={<Contact />}></Route>
         <Route exact path='/' element={<Pricing />}></Route>
       </Routes>
-      </BrowsrRouter>
+      </Router>
     </div>
   );
 }
