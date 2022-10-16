b2be9d584f745b896a6b12bef6d5b31f6cf8a042
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-16 15:52:23 +0200

    pk changes

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -9,9 +9,9 @@ const Header =()=>{
         <Container>
           <Navbar.Brand href="#home">online shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/pricing">Pricing</Nav.Link>
+            <Nav.Link href="/react_deploy/">Home</Nav.Link>
+            <Nav.Link href="/react_deploy/contact">Contact</Nav.Link>
+            <Nav.Link href="/react_deploy/pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar> 
